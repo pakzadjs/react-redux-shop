3d603181700aa4ae960607ac89507e6c5cb6efb2
Arya Pakzad <pakzadlive@gmail.com>
2022-09-11 21:40:54 +0430

    Redux-LocalStorage-Simple

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
+import { save, load } from 'redux-localstorage-simple';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const createStoreWithMiddleware = applyMiddleware(save(), thunk)(createStore);
+
+const store = createStoreWithMiddleware(rootReducer, load());
 
 export default store;
